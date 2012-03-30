c5e352659ba5cb8e2188219487d26eacc416a45a
provemyself <prove_myself@163.com>
2012-03-30 15:59:48 +0800

    Add a new project

--- SaveFish-Android/src/com/savefish/android/SaveFishActivity.java
+++ SaveFish-Android/src/com/savefish/android/SaveFishActivity.java
@@ -3,13 +3,14 @@ package com.savefish.android;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.savefish.service.ApplicationManager;
 
 public class SaveFishActivity extends AndroidApplication {
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		initialize(new ApplicationManager(), false);
+		initialize(new ApplicationManager(this), false);
 	}
+
 }
